c3e1218ab4773eb61b685ff49d78f43b27664529
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-07 20:43:43 +0100

    Create misc2.py

---
+++ DaisyX/modules/misc2.py
@@ -0,0 +1,13 @@
+HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
+
+**/skip** __Skip The Current Playing Music.__
+**/play** __youtube/saavn/deezer Song_Name__
+**/telegram** __Play From Telegram Audio.__
+**/joinvc** __Join Voice Chat.__
+**/leavevc** __Leave Voice Chat.__
+**/listvc** __List Joined Voice Chats.__
+**/volume [1-200]** __Adjust Volume.__
+**/pause** __Pause Music.__
+**/resume** __Resume Music.__
+**/theme** __Change Curently Playing Theme.__
+**/update** __Update & Restart.__"""
