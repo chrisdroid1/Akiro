afe9fb86bba5254e722041ae3a3035186b0b8f7b
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-18 19:56:36 +0100

    Update mongo.py

--- DaisyX/mongo.py
+++ DaisyX/mongo.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio
 from DaisyX import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from YoneRobot.conf import get_int_key, get_str_key
+from DaisyX.conf import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
