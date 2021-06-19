29b07cb67546f1f73e4a53bde8b5b94249ccaec6
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-19 09:08:43 +0100

    Update admin.py

--- DaisyX/modules/admin.py
+++ DaisyX/modules/admin.py
@@ -15,7 +15,7 @@ from DaisyX.modules.helper_funcs.chat_status import (
     user_admin,
     ADMIN_CACHE,
 )
-from DaisyX.helper_extra.admin_rights import (
+from DaisyX.helper_funcs.admin_rights import (
     user_can_pin,
     user_can_promote,
     user_can_changeinfo,
