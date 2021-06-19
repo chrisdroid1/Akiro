f296f178abb9fe9bc3b73b6ddfde5de30be20ca1
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-19 17:10:44 +0100

    Update dbcleanup.py

--- DaisyX/modules/dbcleanup.py
+++ DaisyX/modules/dbcleanup.py
@@ -1,5 +1,9 @@
 from time import sleep
 
+import DaisyX.modules.sql.global_bans_sql as gban_sql
+import DaisyX.modules.sql.users_sql as user_sql
+from DaisyX import DEV_USERS, OWNER_ID, dispatcher
+from DaisyX.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
@@ -8,11 +12,6 @@ from telegram.ext import (
     CommandHandler,
     run_async,
 )
-
-import DaisyX.modules.sql.global_bans_sql as gban_sql
-import DaisyX.modules.sql.users_sql as user_sql
-from DaisyX import DEV_USERS, OWNER_ID, dispatcher
-from DaisyX.modules.helper_funcs.chat_status import dev_plus
 
 
 def get_invalid_chats(update: Update, context: CallbackContext, remove: bool = False):
