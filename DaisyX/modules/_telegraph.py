c4eeb10d3a6900371df95b997983a5fd80ceca2b
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-20 16:03:04 +0100

    Update _telegraph.py

--- DaisyX/modules/_telegraph.py
+++ DaisyX/modules/_telegraph.py
@@ -41,7 +41,7 @@ async def _(event):
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await h.edit("**Uρℓσα∂є∂ тσ [тєℓєgяαρн](https://telegra.ph{})**.\n\n**ву @Akirogrpbot**".format(media_urls[0]), link_preview=True)
+                await h.edit("**Uρℓσα∂є∂ тσ [тєℓєgяαρн](https://telegra.ph{})**.\n\n**ву [Akiro](https://t.me/Akirogrpbot)**".format(media_urls[0]), link_preview=True)
         elif input_str == "t":
             user_object = await tbot.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
