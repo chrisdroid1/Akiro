91cadf9aa8e0d22d371a1e0e841033f77a17dc27
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-20 16:32:12 +0100

    Update _tools.py

--- DaisyX/modules/_tools.py
+++ DaisyX/modules/_tools.py
@@ -1,6 +1,11 @@
 __mod_name__ = "◎Tools"
 
 __help__ = """
+
+*Telegraph*
+ ❍ /txm :Get Telegraph Link Of Replied Media
+
+
 *Converts*
  ❍ /encrypt*:* Encrypts The Given Text
  ❍ /decrypt*:* Decrypts Previously Ecrypted Text
