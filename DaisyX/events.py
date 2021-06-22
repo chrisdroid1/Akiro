ae413184a48432d32d776a83f6472f207866d7ff
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-06-22 07:13:17 +0100

    Update events.py

--- DaisyX/events.py
+++ DaisyX/events.py
@@ -1,137 +1,35 @@
-import glob
 import inspect
+import glob
 import logging
-import re
 import sys
-from pathlib import Path
+import re
 
-from pymongo import MongoClient
+from pathlib import Path
 from telethon import events
 
-from DaisyX import CMD_LIST, LOAD_PLUG, MONGO_DB_URI, tbot, telethn, ubot
+from pymongo import MongoClient
+from DaisyX import MONGO_DB_URI
+from DaisyX import telethn
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI)
-db = client["missjuliarobot"]
+db = client["darkuserbot"]
 gbanned = db.gban
 
-
 def register(**args):
-    pattern = args.get("pattern")
-    r_pattern = r"^[/]"
+    """ Registers a new message. """
+    pattern = args.get("pattern", None)
+
+    r_pattern = r"^[/!]"
 
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
 
     args["pattern"] = pattern.replace("^/", r_pattern, 1)
-    stack = inspect.stack()
-    previous_stack_frame = stack[1]
-    file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
-    reg = re.compile("(.*)")
-
-    if pattern is not None:
-        try:
-            cmd = re.search(reg, pattern)
-            try:
-                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
-            except BaseException:
-                pass
-
-            try:
-                CMD_LIST[file_test].append(cmd)
-            except BaseException:
-                CMD_LIST.update({file_test: [cmd]})
-        except BaseException:
-            pass
 
     def decorator(func):
-        async def wrapper(check):
-            if check.edit_date:
-                return
-            if check.fwd_from:
-                return
-            if check.is_group or check.is_private:
-                pass
-            else:
-                print("i don't work in channels")
-                return
-            users = gbanned.find({})
-            for c in users:
-                if check.sender_id == c["user"]:
-                    return
-            try:
-                await func(check)
-                try:
-                    LOAD_PLUG[file_test].append(func)
-                except Exception:
-                    LOAD_PLUG.update({file_test: [func]})
-            except BaseException:
-                return
-            else:
-                pass
-
-        tbot.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
-
-    return decorator
-
-
-def juliabot(**args):
-    pattern = args.get("pattern", None)
-    disable_edited = args.get("disable_edited", False)
-    ignore_unsafe = args.get("ignore_unsafe", False)
-    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
-    group_only = args.get("group_only", False)
-    args.get("disable_errors", False)
-    insecure = args.get("insecure", False)
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    if "disable_edited" in args:
-        del args["disable_edited"]
-
-    if "ignore_unsafe" in args:
-        del args["ignore_unsafe"]
-
-    if "group_only" in args:
-        del args["group_only"]
-
-    if "disable_errors" in args:
-        del args["disable_errors"]
-
-    if "insecure" in args:
-        del args["insecure"]
-
-    if pattern:
-        if not ignore_unsafe:
-            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
-
-    def decorator(func):
-        async def wrapper(check):
-            if check.edit_date and check.is_channel and not check.is_group:
-                return
-            if group_only and not check.is_group:
-                return
-            if check.via_bot_id and not insecure and check.out:
-                # Ignore outgoing messages via inline bots for security reasons
-                return
-
-            try:
-                await func(check)
-            except events.StopPropagation:
-                raise events.StopPropagation
-            except KeyboardInterrupt:
-                pass
-            except BaseException as e:
-                print(e)
-            else:
-                pass
-
-        if not disable_edited:
-            ubot.add_event_handler(wrapper, events.MessageEdited(**args))
-        ubot.add_event_handler(wrapper, events.NewMessage(**args))
-        return wrapper
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
 
     return decorator
 
@@ -180,11 +78,111 @@ def callbackquery(**args):
     return decorator
 
 
+def bot(**args):
+    pattern = args.get("pattern")
+    r_pattern = r"^[/]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    reg = re.compile("(.*)")
+
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+
+            try:
+                FUN_LIST[file_test].append(cmd)
+            except BaseException:
+                FUN_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date:
+                return
+            if check.fwd_from:
+                return
+            if check.is_group or check.is_private:
+                pass
+            else:
+                print("i don't work in channels")
+                return
+            if check.is_group:
+               if check.chat.megagroup:
+                  pass
+               else:
+                  print("i don't work in small chats")
+                  return
+                          
+            users = gbanned.find({})
+            for c in users:
+                if check.sender_id == c["user"]:
+                    return
+            try:
+                await func(check)
+                try:
+                    LOAD_PLUG[file_test].append(func)
+                except Exception:
+                    LOAD_PLUG.update({file_test: [func]})
+            except BaseException:
+                return
+            else:
+                pass
+
+        telethn.add_event_handler(wrapper, events.NewMessage(**args))
+        return wrapper
+
+    return decorator
+
+
+def DaisyX(**args):
+    pattern = args.get("pattern", None)
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    group_only = args.get("group_only", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "group_only" in args:
+        del args["group_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern:
+        if not ignore_unsafe:
+            args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
+
+
 def load_module(shortname):
     if shortname.startswith("__"):
         pass
     elif shortname.endswith("_"):
         import importlib
+        import DaisyX.events
 
         path = Path(f"DaisyX/modules/{shortname}.py")
         name = "DaisyX.modules.{}".format(shortname)
@@ -194,14 +192,15 @@ def load_module(shortname):
         print("Successfully imported " + shortname)
     else:
         import importlib
+        import DaisyX.events
 
         path = Path(f"DaisyX/modules/{shortname}.py")
         name = "DaisyX.modules.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.register = register
-        mod.juliabot = juliabot
-        mod.tbot = tbot
+        mod.DaisyX = DaisyX
+        mod.tbot = telethn
         mod.logger = logging.getLogger(shortname)
         spec.loader.exec_module(mod)
         sys.modules["DaisyX.modules." + shortname] = mod
